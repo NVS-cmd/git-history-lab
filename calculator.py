8fb337860941c21f796417100411aeb0908ea01d
NVS-cmd <nagornyyyvs@student.bmstu.ru>
2025-12-15 13:05:45 +0300

    Добавлены функции умножения и вычитания

--- calculator.py
+++ calculator.py
@@ -2,4 +2,12 @@ def add(a, b):
 	return a + b
 
 def divide(a, b):
+	if b == 0:
+		raise ValueError("Деление на ноль невозможно")
 	returm a / b
+
+def multiply(a, b):
+	return a * b
+
+def subtract(a, b):
+	return a - b
